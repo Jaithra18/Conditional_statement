460b1a9b1f2c8c9c3569e7f9537f6b9e0920abe9
Jaithra18 <Jaithrathota@gmail.com>
2024-09-16 18:15:43 +0100

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,7 @@
+a=float(input("enter a price"))
+s=float(input("enter a selling price"))
+p= s-a
+if s>a:
+    print("The profit amount is",p)
+else:
+    print("Thats a loss!")
---
+++ main5.py
@@ -0,0 +1,6 @@
+a=int(input("Enter a number"))
+if a>15:
+    print("Greater than 15")
+else:
+    print("Lower than 15")
+    
